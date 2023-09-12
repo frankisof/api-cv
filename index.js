47f9eeea5e6783b2b4cce6eb8ef71b2a949de7e5
IrvingCM123 <98989957+IrvingCM123@users.noreply.github.com>
2023-09-11 21:29:21 -0600

    cambios

---
+++ index.js
@@ -0,0 +1,26 @@
+const cors = require('cors')
+const express = require('express')
+
+
+const app = express();
+app.use(cors());
+app.options('*', cors());
+
+const port = 8080;
+
+app.get('/', (req, res, next) => {
+	res.send('node express api <br> by adsoft');
+});
+
+
+app.get('/header', (req, res, next) => {
+  res.sendfile("assets/header.json");
+});
+
+app.get('/workexperience', (req, res, next) => {
+	res.sendfile("assets/work.json");
+  });
+
+app.listen(port,  () => 
+	console.log('listening on port ' + port
+));
